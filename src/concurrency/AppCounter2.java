4db13bb2ca17f4d8c39058c9197b1eb08c7ab054
Samuel <samugit.projects@gmail.com>
2024-03-07 14:54:13 -0300

    Aula08 Usando o método join() para aguardar outras threads

--- src/concurrency/AppCounter2.java
+++ src/concurrency/AppCounter2.java
@@ -1,8 +1,14 @@
 package concurrency;
 
+/*
+* Para bloquear a thread principal até que outras threads finalizem pode usar o join() para isso
+* O join faz com que a thread que o chamou fique bloqueada até o termino da nova thread
+*
+* */
+
 public class AppCounter2 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         SharedCounter c = new SharedCounter();
 
@@ -11,5 +17,10 @@ public class AppCounter2 {
 
         Thread t2 = new Thread(c);
         t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("End"); // após t1 e t2 terminarem a thread principal vai imprimir na tela
     }
 }
