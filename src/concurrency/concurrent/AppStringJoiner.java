5bd803cf038570e87e64bce09f8ae74f558b2baf
Samuel <samugit.projects@gmail.com>
2024-03-20 11:05:58 -0300

    Aula37 Escolhendo entre execute() e submit()

--- src/concurrency/concurrent/AppStringJoiner.java
+++ src/concurrency/concurrent/AppStringJoiner.java
@@ -12,7 +12,8 @@ public class AppStringJoiner {
 
         try (ExecutorService e = Executors.newSingleThreadExecutor()) {
             StringJoinerRunnable runnable = new StringJoinerRunnable(List.of("A", "B", "C"));
-            Future<?> future = e.submit(runnable);
+            //e.execute(runnable); // Faz o mesmo que o submit, porém, não retorna nada. Pode ser utilizado quando não se importa com o sincronismo da tarefa
+            Future<?> future = e.submit(runnable); // submit retorna um Future com a possível resposta futura que pode ser entregue
             future.get(); // thread principal bloqueia até que exista uma resposta da thread que está no ExecutorService
             System.out.println(runnable.getResult()); // depois desbloqueia e pega o resultado (união das Strings) que estava na thread a parte
 
