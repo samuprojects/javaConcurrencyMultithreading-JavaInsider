1ab5005d142b0dbd3eca0c69ea55a4978e0c371f
Samuel <samugit.projects@gmail.com>
2024-03-20 11:33:07 -0300

    Aula39 Propagação de exceções na interface Callable

--- src/concurrency/concurrent/AppStringJoiner2.java
+++ src/concurrency/concurrent/AppStringJoiner2.java
@@ -1,21 +1,25 @@
 package concurrency.concurrent;
 
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+@SuppressWarnings("ALL")
 public class AppStringJoiner2 {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
 
         try (ExecutorService e = Executors.newSingleThreadExecutor()) {
-            Future<String> future = e.submit(new StringJoinerCallable(List.of("A", "B", "C")));
-            System.out.println(future.get());
+            Future<String> future = e.submit(new StringJoinerCallable(null));
 
-        } catch (ExecutionException e) {
-            throw new RuntimeException(e);
+            try {
+                System.out.println(future.get());
+            } catch (ExecutionException ex) { // exemplo de como identificar se o Callable lançou exceção durante a execução
+                Throwable cause = ex.getCause();
+                System.out.println("ERROR: " + cause.getMessage());
+                cause.printStackTrace();
+            }
         }
     }
 }
